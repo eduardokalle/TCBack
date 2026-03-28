1be28e5596a96368f637ec26be32582397d0dfef
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2026-03-28 00:32:46 -0500

    fix .yml

--- app.js
+++ app.js
@@ -7,32 +7,32 @@
 require('dotenv').config()
 const { express } = require("./App/config");
 const { serve } = require("./server");
-const https = require('https'); // https
-const fs = require('fs'); // https
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const cors = require('cors');
 
 let app = new express();
 
 app.use(cors());
 
-const sslFolder = '/etc/letsencrypt/live/tucarrera.co/'
-
-//const privateKey = fs.readFileSync( '/etc/ssl/private/tucarrera_co.key' ); // https
-//const certificate = fs.readFileSync( '/etc/ssl/certs/tucarrera_co.crt' ); // https
-//const caChain = fs.readFileSync( '/etc/apache2/ssl.crt/tucarrera_co.ca-bundle' ); // https
-
-const privateKey = fs.readFileSync(`${sslFolder}privkey.pem`);
-const certificate = fs.readFileSync(`${sslFolder}fullchain.pem`);
-
 serve(app);
 
-https.createServer({	 // https
-    key: privateKey,	 // https
-    cert: certificate,	 // https
-}, app).listen(app.get('port'), () => {
+const useHttp = process.env.USE_HTTP === '1' || process.env.USE_HTTP === 'true';
+
+const onListen = () => {
     try {
         console.log(`El Back-End de TuCarrera.site está corriendo en el puerto: ${app.get('port')}`);
     } catch (error) {
         console.log(`App error: ${error}`);
     }
-});
+};
+
+if (useHttp) {
+    http.createServer(app).listen(app.get('port'), onListen);
+} else {
+    const sslFolder = '/etc/letsencrypt/live/tucarrera.co/'
+    const privateKey = fs.readFileSync(`${sslFolder}privkey.pem`);
+    const certificate = fs.readFileSync(`${sslFolder}fullchain.pem`);
+    https.createServer({ key: privateKey, cert: certificate }, app).listen(app.get('port'), onListen);
+}
